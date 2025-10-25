52c843e71e821dea106b61d1f868da4d94f6d17a
mehul.chopada@bridgestone.com <mehul.chopada@bridgestone.com>
2025-10-25 10:45:40 +0200

    Set allowedHosts to true to allow all hosts in preview mode

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
       preview: {
         port: 4173,
         host: '0.0.0.0',
-        allowedHosts: ['gmg-frontend.onrender.com']
+        allowedHosts: true
       },
       plugins: [react()],
       define: {
